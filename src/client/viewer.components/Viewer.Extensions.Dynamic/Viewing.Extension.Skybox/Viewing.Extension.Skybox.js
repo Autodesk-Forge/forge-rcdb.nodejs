479509d6cbf806cce701045181699bace85fd7bf
Philippe Leefsma <philippe.leefsma@gmail.com>
2017-07-15 12:00:37 +0800

    skybox update

--- src/client/viewer.components/Viewer.Extensions.Dynamic/Viewing.Extension.Skybox/Viewing.Extension.Skybox.js
+++ src/client/viewer.components/Viewer.Extensions.Dynamic/Viewing.Extension.Skybox/Viewing.Extension.Skybox.js
@@ -10,7 +10,9 @@ import ypos from './img/bridge/skybox-ypos.png'
 import yneg from './img/bridge/skybox-yneg.png'
 import zpos from './img/bridge/skybox-zpos.png'
 import zneg from './img/bridge/skybox-zneg.png'
+import EventTool from 'Viewer.EventTool'
 import Skybox from 'Viewer.Skybox'
+import Stopwatch from 'Stopwatch'
 
 class SkyboxExtension extends MultiModelExtensionBase {
 
@@ -21,6 +23,40 @@ class SkyboxExtension extends MultiModelExtensionBase {
   constructor(viewer, options) {
 
     super (viewer, options)
+
+    this.onUserInteraction =
+      this.onUserInteraction.bind(this)
+
+    this.runAnimation =
+      this.runAnimation.bind(this)
+
+    this.eventTool = new EventTool(viewer)
+
+    this.eventTool.on('mousewheel', (e) => {
+
+      return true
+    })
+
+    this.eventTool.on('buttondown', (e) => {
+
+      window.clearTimeout(this.timeoutId)
+
+      this.userInteraction = true
+
+      return false
+    })
+
+    this.eventTool.on('buttonup', (e) => {
+
+      this.timeoutId = window.setTimeout(() => {
+        this.stopwatch.getElapsedMs()
+        this.runAnimation()
+      }, 3500)
+
+      this.userInteraction = false
+
+      return false
+    })
 
     const imageList = [
       xpos, xneg,
@@ -36,6 +72,8 @@ class SkyboxExtension extends MultiModelExtensionBase {
       imageList,
       size
     })
+
+    this.stopwatch = new Stopwatch()
   }
 
   /////////////////////////////////////////////////////////
@@ -62,32 +100,45 @@ class SkyboxExtension extends MultiModelExtensionBase {
   //
   //
   /////////////////////////////////////////////////////////
-  onModelCompletedLoad () {
+  onModelCompletedLoad (event) {
 
-    this.viewer.navigation.toPerspective()
+    if (event.model.dbModelId) {
 
-    this.loadContainer().then(() => {
+      this.viewer.navigation.toPerspective()
 
-      this.options.loader.show(false)
-    })
+      this.loadContainer(this.options.containerURN).then(
+        () => {
+          this.options.loader.show(false)
+        })
+
+      const pos = this.viewer.navigation.getPosition()
+
+      this.viewer.navigation.setPosition(
+        new THREE.Vector3(pos.x, pos.y - 100, pos.z))
+
+      this.stopwatch.getElapsedMs()
+
+      this.eventTool.activate()
+
+      this.runAnimation()
+    }
   }
 
   /////////////////////////////////////////////////////////
   //
   //
   /////////////////////////////////////////////////////////
-  loadContainer () {
+  loadContainer (urn) {
 
     return new Promise(async(resolve) => {
 
-      const doc = await this.options.loadDocument(
-        this.options.containerURN)
+      const doc = await this.options.loadDocument(urn)
 
       const path = this.options.getViewablePath(doc)
 
       this.viewer.loadModel(path, {}, (model) => {
 
-        resolve(model)
+        resolve (model)
       })
     })
   }
@@ -99,6 +150,56 @@ class SkyboxExtension extends MultiModelExtensionBase {
   unload() {
 
     console.log('Viewing.Extension.Skybox unloaded')
+
+    window.cancelAnimationFrame(this.animId)
+
+    this.userInteraction = true
+
+    this.eventTool.off()
+  }
+
+  /////////////////////////////////////////////////////////
+  //
+  //
+  /////////////////////////////////////////////////////////
+  onUserInteraction () {
+
+    console.log('onUserInteraction')
+  }
+
+  /////////////////////////////////////////////////////////
+  //
+  //
+  /////////////////////////////////////////////////////////
+  rotateCamera (axis, speed, dt) {
+
+    const pos = this.viewer.navigation.getPosition()
+
+    const matrix = new THREE.Matrix4().makeRotationAxis(
+      axis, speed * dt);
+
+    pos.applyMatrix4(matrix)
+
+    this.viewer.navigation.setPosition(pos)
+  }
+
+  /////////////////////////////////////////////////////////
+  //
+  //
+  /////////////////////////////////////////////////////////
+  runAnimation () {
+
+    if (!this.userInteraction) {
+
+      const dt = this.stopwatch.getElapsedMs() * 0.001
+
+      const axis = new THREE.Vector3(0,1,0)
+
+      this.rotateCamera(axis, 10.0 * Math.PI/180, dt)
+
+      this.animId = window.requestAnimationFrame(
+        this.runAnimation)
+    }
   }
 }
 
