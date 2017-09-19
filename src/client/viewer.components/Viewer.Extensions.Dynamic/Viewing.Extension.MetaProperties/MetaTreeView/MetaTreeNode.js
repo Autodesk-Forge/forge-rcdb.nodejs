4a30089cf4bfe2eb0c3472e36ca43d64782ca7fa
Philippe Leefsma <philippe.leefsma@gmail.com>
2017-09-20 01:13:59 +0200

    fix

--- src/client/viewer.components/Viewer.Extensions.Dynamic/Viewing.Extension.MetaProperties/MetaTreeView/MetaTreeNode.js
+++ src/client/viewer.components/Viewer.Extensions.Dynamic/Viewing.Extension.MetaProperties/MetaTreeView/MetaTreeNode.js
@@ -1,7 +1,7 @@
 import EventsEmitter from 'EventsEmitter'
 import Spinner from 'react-spinkit'
 import PropTypes from 'prop-types'
-import DOMPurify from 'dompurify'
+import ReactDOM from 'react-dom'
 import Label from 'Label'
 import React from 'react'
 
